65c399ef15ea97fce91704eab4ce0364de0b6a03
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-10-09 11:12:10 +0530

    Refactor socialMediaPost routes to include authentication middleware

--- routes/socialMediaPost.route.js
+++ routes/socialMediaPost.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { createSocialMediaPostController, listSocialMediaPostController, getSocialMediaPostById, updateSocialMediaPostController, deleteSocialMediaPostController } = require('../controller/socialMediaPost.controller');
 const { validationMiddleware } = require('../middlewares/validation.middleware');
 const { createSocialMediaPostBodySchema } = require('../validation-schemas/socialMediaPost.validation-schema');
+const { verifyMiddleware } = require('../controller/auth.controller');
 
 const socialMediaPostRouter = express.Router();
 
 socialMediaPostRouter.post('/',validationMiddleware(createSocialMediaPostBodySchema) ,createSocialMediaPostController) // create socialMediaPost
 
-socialMediaPostRouter.get('/',listSocialMediaPostController)
+socialMediaPostRouter.get('/', verifyMiddleware, listSocialMediaPostController)
 // get single socialMediaPost
 socialMediaPostRouter.get('/:socialMediaPostId', getSocialMediaPostById)
 socialMediaPostRouter.put('/:socialMediaPostId', updateSocialMediaPostController)
